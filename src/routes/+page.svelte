7fc2d92aee08279b75859330f4ae73b14d40d56a
Alexandre Costa <alexandre97costa@gmail.com>
2023-11-03 19:15:12 +0000

    login funciona!
    14:39

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+	import '../app.postcss';
+
+	export let data;
+</script>
+
 <main>
 	<h1>SvelteKit & Supabase Auth</h1>
-	<p>Let's learn how to register and login users!</p>
-	<div class="auth-buttons">
-		<a href="/login" class="btn btn-primary">Login</a>
-		<a href="/register" class="btn btn-secondary">Register</a>
-	</div>
+	{#if data.session}
+		<p>Welcome, {data.session.user.email}</p>
+	{:else}
+		<p>Let's learn how to register and login users!</p>
+		<div class="auth-buttons">
+			<a href="/login" class="btn btn-primary">Login</a>
+			<a href="/register" class="btn btn-secondary">Register</a>
+		</div>
+	{/if}
 </main>
