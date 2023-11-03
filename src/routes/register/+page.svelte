dd7899f8c8d45c1a76a7e06d97887f5aaacfa335
Alexandre Costa <alexandre97costa@gmail.com>
2023-11-03 19:04:35 +0000

    registration
    12:46

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<main>
+    <h1>Register</h1>
+    <form action="?/register" method="post" class="auth-form">
+        <label for="">Email</label>
+        <input type="text" name="email">
+        <label for="">Password</label>
+        <input type="password" name="password">
+        
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+</main>
